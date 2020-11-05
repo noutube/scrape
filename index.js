b7b902cee123755e83665d823e62d741924d8dbe
Jake Barnes <me@jakebarn.es>
2020-11-06 10:32:55 +1100

    Initial commit with duration scraping

---
+++ index.js
@@ -0,0 +1,86 @@
+const axios = require('axios');
+
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' });
+
+const {
+  LAMBDA_FUNCTION_NAME,
+  TOKEN
+} = process.env;
+
+const forceColdStart = async () => {
+  const timestamp = new Date().toISOString();
+  console.log('force cold start', LAMBDA_FUNCTION_NAME, timestamp);
+
+  const currentFunctionConfiguration = await lambda.getFunctionConfiguration({ FunctionName: LAMBDA_FUNCTION_NAME }).promise();
+
+  await lambda.updateFunctionConfiguration({
+    FunctionName: LAMBDA_FUNCTION_NAME,
+    Environment: {
+      Variables: {
+        ...currentFunctionConfiguration.Environment.Variables,
+        TIMESTAMP: timestamp
+      }
+    }
+  }).promise();
+};
+
+const getData = async (url) => {
+  try {
+    const { data } = await axios.get(url, {
+      headers: {
+        'X-YouTube-Client-Name': '1',
+        'X-YouTube-Client-Version': '2.20200214.04.00'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log('failed to get data', error);
+    if (error.response && error.response.status === 429) {
+      forceColdStart();
+    }
+    throw error;
+  }
+};
+
+const getDuration = (data) => {
+  try {
+    // YouTube randomly picks different response formats
+    const playerResponse = data[2].playerResponse || JSON.parse(data[2].player.args.player_response);
+    return parseInt(playerResponse.videoDetails.lengthSeconds, 10);
+  } catch (error) {
+    // allow us to debug new formats
+    console.log('failed to get duration', error, JSON.stringify(data, null, 2));
+    throw error;
+  }
+};
+
+exports.handler = async function(event, context) {
+  console.log('event', event);
+
+  const { token } = event.queryStringParameters;
+  if (token !== TOKEN) {
+    return {
+      statusCode: 401
+    };
+  }
+
+  const { routeKey } = event;
+  if (routeKey === 'GET /duration') {
+    const { videoId } = event.queryStringParameters;
+    const data = await getData(`https://www.youtube.com/watch?v=${videoId}&pbj=1`);
+    const duration = getDuration(data);
+    console.log('duration', duration);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ duration }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  }
+
+  return {
+    statusCode: 404
+  };
+};
